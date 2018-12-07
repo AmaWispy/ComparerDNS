7ac49cadda6b2884292c3032de070775c2174b5d
Wispy <nowispy@mail.ru>
2018-12-07 12:44:41 +0300

    Вывод в textBox3 всеъ товаров

--- ComparerDNS/Form1.Designer.cs
+++ ComparerDNS/Form1.Designer.cs
@@ -33,48 +33,66 @@
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.textBox2 = new System.Windows.Forms.TextBox();
             this.textBox3 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(36, 108);
+            this.button1.Location = new System.Drawing.Point(12, 77);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.Size = new System.Drawing.Size(106, 23);
             this.button1.TabIndex = 1;
-            this.button1.Text = "button1";
+            this.button1.Text = "Анализ файлов";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(44, 116);
+            this.textBox1.Location = new System.Drawing.Point(12, 12);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.Size = new System.Drawing.Size(257, 20);
             this.textBox1.TabIndex = 2;
+            this.textBox1.Text = "Имя файла для анализа 1";
+            this.textBox1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.textBox1_MouseDown);
             // 
             // textBox2
             // 
-            this.textBox2.Location = new System.Drawing.Point(228, 164);
+            this.textBox2.Location = new System.Drawing.Point(12, 51);
             this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.Size = new System.Drawing.Size(257, 20);
             this.textBox2.TabIndex = 3;
+            this.textBox2.Text = "Имя файла для анализа 2";
+            this.textBox2.MouseDown += new System.Windows.Forms.MouseEventHandler(this.textBox2_MouseDown);
             // 
             // textBox3
             // 
-            this.textBox3.Location = new System.Drawing.Point(228, 12);
+            this.textBox3.Location = new System.Drawing.Point(275, 12);
+            this.textBox3.Multiline = true;
             this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(100, 20);
+            this.textBox3.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.textBox3.Size = new System.Drawing.Size(548, 426);
             this.textBox3.TabIndex = 4;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(124, 77);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(145, 23);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "Сохранение в документ";
+            this.button2.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(646, 450);
+            this.ClientSize = new System.Drawing.Size(835, 450);
+            this.Controls.Add(this.button2);
             this.Controls.Add(this.textBox3);
             this.Controls.Add(this.textBox2);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "Form1";
             this.Text = "ComparerDNS";
@@ -88,6 +106,7 @@
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.TextBox textBox2;
         private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Button button2;
     }
 }
 
--- ComparerDNS/Form1.cs
+++ ComparerDNS/Form1.cs
@@ -7,11 +7,23 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
+using System.Threading;
 
 namespace ComparerDNS
 {
     public partial class Form1 : Form
     {
+        public const string t = ".txt";
+        public string[] file2Text = new string[127];
+        public string[] file1Text = new string[127];
+        public StreamReader[] file = new StreamReader[3];
+        public int countFile = 0;
+        public Thread transferToStringThr;
+        public delegate void TxtMaker(string getText);
+        TxtMaker txtMaker;
+        public bool stopThr = false;
+
         public Form1()
         {
             InitializeComponent();
@@ -19,7 +31,75 @@ namespace ComparerDNS
 
         private void button1_Click(object sender, EventArgs e)
         {
+            if (textBox1.Text != "" && textBox2.Text != "" && textBox1.Text != "Имя файла для анализа 1" && textBox2.Text != "Имя файла для анализа 2")
+            {
+                if (File.Exists(textBox1.Text + t) && File.Exists(textBox2.Text + t))
+                {
+                    txtMaker = new TxtMaker(TxtMakerMethod);
+                    countFile = 0;
+                    file[1] = new StreamReader(textBox1.Text + t);
+                    file[2] = new StreamReader(textBox2.Text + t);
+                    countFile++;
+                    transferToStringThr = new Thread(new ThreadStart(TransferToString));
+                    transferToStringThr.Start();
+                    Thread thrWaiterThr = new Thread(new ThreadStart(ThrWaiter));
+                    thrWaiterThr.Start();
+                }
+                else { MessageBox.Show("Указанных вами файлов не существует в папке с программой","System.Error"); }
+            }
+            else { MessageBox.Show("Введите имя файлов без расширения", "System.Error"); }
+        }
 
+        void TransferToString()
+        {
+            int i = 0;
+            if (countFile == 1)
+            {
+                while (!file[countFile].EndOfStream)
+                {
+                    file1Text[i] = file[countFile].ReadLine();
+                    Invoke(txtMaker, file1Text[i]);
+                    //textBox3.Text = (fileText[saverCountFile][i] + Environment.NewLine);
+                    i++;
+                }
+            }
+            else
+            {
+                while (!file[countFile].EndOfStream)
+                {
+                    file2Text[i] = file[countFile].ReadLine();
+                    Invoke(txtMaker, file2Text[i]);
+                    //textBox3.Text = (fileText[saverCountFile][i] + Environment.NewLine);
+                    i++;
+                }
+            }
+            file[countFile].Close();
+            MessageBox.Show("Первый кончился");
+        }
+
+        void ThrWaiter()
+        {
+            while (transferToStringThr.IsAlive && !stopThr)
+            {
+                stopThr = true;
+                countFile++;
+                transferToStringThr = new Thread(new ThreadStart(TransferToString));
+                transferToStringThr.Start();
+            }
+        }
+        void TxtMakerMethod(string getText)
+        {
+            textBox3.AppendText(getText + Environment.NewLine);
+        }
+
+        private void textBox1_MouseDown(object sender, MouseEventArgs e)
+        {
+            textBox1.Text = null;
+        }
+
+        private void textBox2_MouseDown(object sender, MouseEventArgs e)
+        {
+            textBox2.Text = null;
         }
     }
 }
